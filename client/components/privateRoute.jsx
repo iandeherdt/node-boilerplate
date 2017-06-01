fca9c05142bdca3c5b6b9cf722098e9bfbe0b4fc
Ian De Herdt <ian.deherdt@it-erate.be>
2017-06-01 21:32:49 +0200

    pass store to router for authentication

---
+++ client/components/privateRoute.jsx
@@ -0,0 +1,26 @@
+import { Redirect, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import React from 'react';
+const PrivateRoute = ({ component: Component, store: store, ...rest }) => (
+  <Route {...rest} render={props => {
+    if(store.getState().user.get('isAuthenticated')){
+      return <Component {...props}/>;
+    }else {
+      return (<Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }}/>);
+    }
+  }}/>
+);
+
+PrivateRoute.propTypes = {
+  location: PropTypes.string,
+  component: PropTypes.func,
+  store: PropTypes.object.isRequired
+};
+
+PrivateRoute.displayName = 'PrivateRoute';
+
+export default PrivateRoute;
+
---
+++ client/containers/oAuthSuccessContainer.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class OAuthSuccessContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (<div className="container">
+      <div className="light-background large-padding">
+        <p>OAuthSuccess</p> {//eslint-disable-line
+          }
+      </div>
+    </div>);
+  }
+}
+
+export default OAuthSuccessContainer;
