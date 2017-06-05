fd2d854dd9b32792a0c42db58cba17bd062c5181
Ian De Herdt <ian.deherdt@it-erate.be>
2017-06-05 13:32:14 +0200

    add missing files

---
+++ client/components/authComplete.jsx
@@ -0,0 +1,33 @@
+import { Redirect, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+import queryString from 'query-string';
+import { oAuthLoginSuccess } from '../actions/user';
+const AuthComplete = ({ ...rest }) => {
+  return (<Route {...rest} render={props => {
+    const parsed = queryString.parse(props.location.search);
+    if(parsed.token){
+      rest.dispatch(oAuthLoginSuccess({name: parsed.user}, parsed.token));
+      return (<Redirect to={{
+        pathname: '/',
+        state: { from: props.location }
+      }}/>);
+    }else {
+      return (<Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }}/>);
+    }
+  }}/>);
+};
+
+AuthComplete.propTypes = {
+  location: PropTypes.string,
+  component: PropTypes.func,
+  match: PropTypes.object
+};
+
+AuthComplete.displayName = 'AuthComplete';
+
+export default connect()(AuthComplete);
