42151ef6a28850d3be7b5b15e77dacbb51e355ad
Ian De herdt <iandeherdt@Ians-MBP.home>
2017-08-10 21:41:53 +0200

    add parallax to homepage

--- client/containers/homeContainer.jsx
+++ client/containers/homeContainer.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import ContentSection from '../components/contentSection.jsx';
+import { Parallax } from 'react-parallax';
 const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vitae risus at mauris pretium eleifend. Vivamus interdum sodales bibendum. Nam feugiat at sapien et euismod. Aliquam cursus ipsum nec sem efficitur blandit. Fusce non ligula eget lacus semper finibus id eget lorem. Nulla sit amet viverra ligula. Donec gravida, sapien a laoreet tristique, tortor magna venenatis quam, sed pulvinar ante lectus placerat leo. Sed elementum, mi non laoreet semper, odio justo condimentum turpis, id pulvinar urna arcu eget mi. Phasellus efficitur, urna nec aliquam aliquam, tortor elit dapibus purus, rhoncus tempus lorem lacus nec neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.";
 class HomeContainer extends Component {
   constructor(props) {
@@ -11,18 +12,50 @@ class HomeContainer extends Component {
   render() {
     return (
         <div className="full-width" style={{alignSelf: 'baseline'}}>
-          <ContentSection transparent>
-            <h2>I'M A BANNER</h2>
-            <div>{lorem}</div>
-          </ContentSection>
-          <ContentSection>
-            <h2>I'M SOME CONTENT 1</h2>
-            <div>{lorem}</div>
-          </ContentSection>
-          <ContentSection>
-            <h2>I'M SOME CONTENT 2</h2>
-            <div>{lorem}</div>
-          </ContentSection>
+          <Parallax bgImage={require('../images/background.jpeg')} strength={400}>
+            <ContentSection transparent>
+              <h2>I'M A BANNER</h2>
+              <div>{lorem}</div>
+            </ContentSection>
+            <ContentSection>
+              <h2>I'M SOME CONTENT 1</h2>
+              <div>{lorem}</div>
+            </ContentSection>
+            <ContentSection>
+              <h2>I'M SOME CONTENT 2</h2>
+              <div>{lorem}</div>
+            </ContentSection>
+            <ContentSection transparent>
+              <h2>I'M SOME CONTENT 2</h2>
+              <div>{lorem}</div>
+            </ContentSection>
+            <ContentSection>
+              <h2>I'M SOME CONTENT 2</h2>
+              <div>{lorem}</div>
+            </ContentSection>
+          </Parallax>
+          <Parallax bgImage={require('../images/background2.jpeg')} strength={400}>
+            <ContentSection transparent>
+              <h2>I'M A BANNER</h2>
+              <div>{lorem}</div>
+            </ContentSection>
+            <ContentSection>
+              <h2>I'M SOME CONTENT 1</h2>
+              <div>{lorem}</div>
+            </ContentSection>
+            <ContentSection>
+              <h2>I'M SOME CONTENT 2</h2>
+              <div>{lorem}</div>
+            </ContentSection>
+            <ContentSection transparent>
+              <h2>I'M SOME CONTENT 2</h2>
+              <div>{lorem}</div>
+            </ContentSection>
+            <ContentSection>
+              <h2>I'M SOME CONTENT 2</h2>
+              <div>{lorem}</div>
+            </ContentSection>
+          </Parallax>
         </div>);
   }
 }
