d9354e9772e504ac5de72014e060cb50288c22a3
Ian De herdt <ian.deherdt@icapps.com>
2017-09-07 21:39:52 +0200

    remove obsolete plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const extractSass = new ExtractTextPlugin({
-  filename: '[name].[contenthash].css',
+  filename: '[name].css',
   disable: false
 });
 
